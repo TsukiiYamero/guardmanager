0c0d848b5f8bdf219f506329f79c92fbc008a2b9
SaiKisho <diegosferas@gmail.com>
2024-07-05 16:41:56 -0500

    test https

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 /* configuracion de la conexion de la api */
 
 export const API_CONFIG = {
-    baseUrl: 'http://ec2-34-203-40-176.compute-1.amazonaws.com',
+    baseUrl: 'https://ec2-34-203-40-176.compute-1.amazonaws.com',
     api: "/api",
     login: '/login',
     logout: '/logout',
